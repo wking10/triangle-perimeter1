1869f796dda3eac3b935ac8a6b12d8421d72df2b
wking10 <112655433+wking10@users.noreply.github.com>
2022-09-23 12:30:35 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+// Triangle Perimeter Assignment Start Code
+
+// Calculate Button
+document.getElementById('btn').addEventListener('click', btnClicked);
+
+function btnClicked() {
+
+// GetInputs
+let XA = +document.getElementById('XA').value;
+let YA = +document.getElementById('YA').value;
+let XB = +document.getElementById('XB').value;
+let YB = +document.getElementById('YB').value;
+let XC = +document.getElementById('XC').value;
+let YC = +document.getElementById('YC').value;
+
+// Calculate side lengths
+let AB = dist(XA,YA,XB,YB)
+let AC = dist(YA,XA,YC,XC)
+let BC = dist(YB,XB,XC,YC)
+
+// Calculate 
+document.getElementById("AB-ans").innerHTML = AB
+document.getElementById("AC-ans").innerHTML = AC
+document.getElementById("BC-ans").innerHTML = BC
+document.getElementById("Perimeter-ans").innerHTML = AB+AC+BC
+
+}
+
+function dist(xone, yone, xtwo, ytwo) { 
+return Math
+}
+
